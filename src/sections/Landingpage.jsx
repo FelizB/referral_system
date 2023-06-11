17cc2719f34537831b3310add0e965a9ee86f551
Felix <bosirefelix97@gmail.com>
2023-06-12 00:19:12 +0300

    landing decorations

--- src/sections/Landingpage.jsx
+++ src/sections/Landingpage.jsx
@@ -3,31 +3,36 @@ import "../components/LandingComponents/css/landing.css"
 import Navbar from "../components/LandingComponents/navbar";
 import Features from "../components/LandingComponents/Features";
 import Land from "../components/LandingComponents/Land";
+import About from "../components/LandingComponents/About";
 import Testimonial from "../components/LandingComponents/Testimonial";
 import Partners from "../components/LandingComponents/Partners";
 import Footer from "../components/LandingComponents/footer";
 import Faq from "../components/LandingComponents/Faq";
 
 function Landingpage(){
+    
     return(
     <div className="body">
         <Navbar />
-        <div className="landing-container">
+        <div className="landing-container" id="Land">
             <Land />
         </div>
-        <div className="features-container">
+        <div className="features-container" id="Features">
             <Features />
         </div>
-        <div className="testimoninal-container">
+        <div className="About-container" id="About">
+            <About />
+        </div>
+        <div className="testimoninal-container" id="Testimonial">
             <Testimonial />
         </div>
-        <div className="faq-contained">
+        <div className="faq-contained" id="Faq">
             <Faq />
         </div>
-        <div className="partners-container">
+        <div className="partners-container" id="Partners">
             <Partners />
         </div>
-        <div className="footer-container">
+        <div className="footer-container" id="Footer">
             <Footer />
         </div>
     </div>
